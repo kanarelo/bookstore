6f78b0eb4d417585b91792d5b2a7f8c2f630a756
Mukewa O. Wekalao <onesmus.mukewa@gmail.com>
2021-04-07 08:20:26 +0300

    moves models.py and tests.py into books app

--- books/models.py
+++ books/models.py
@@ -1,3 +1,50 @@
-from django.db import models
 
-# Create your models here.
+class Customer(object):
+    def __init__(self, name=""):
+        self.name = name
+
+class Book(object):
+    REGULAR = "regular"
+    FICTION = "fiction"
+    NOVEL   = "novel"
+
+    def __init__(self, name="", kind=REGULAR):
+        self.name = name
+        self.kind = kind
+
+
+    def get_rental_charge(self, days_borrowed):
+        if self.kind == Book.REGULAR:
+            # Regular books for the first 2 days charges 
+            # will be $1 per day and $1.5 thereafter
+
+            if days_borrowed > 2:
+                return (2 * 1) + ((days_borrowed - 2) * 1.5)
+            else:
+                return (days_borrowed * 1)
+
+        elif self.kind == Book.FICTION:
+            # Minimum changes will b3 considered as $2 
+            # if days rented is less than 2 days.
+
+            daily_charge   = 3.0
+            minimum_charge = 2.0
+
+            if days_borrowed <= 2:
+                return minimum_charge
+            else:
+                return (days_borrowed * daily_charge)
+
+        elif self.kind == Book.NOVEL:
+            # Novel minimum charges are introduced as $4.5 
+            # if days rented is less than 3 days
+
+            daily_charge   = 1.5
+            minimum_charge = 4.5
+
+            if days_borrowed <= 3:
+                return minimum_charge
+            else:
+                return (days_borrowed * daily_charge)
+
+        
