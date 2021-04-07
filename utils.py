fc32674b38390a6262273d3030af1c89d9ed6e9c
Mukewa O. Wekalao <onesmus.mukewa@gmail.com>
2021-04-07 06:42:51 +0300

    Simplify calculation

--- utils.py
+++ utils.py
@@ -1,9 +1,5 @@
 from models import Book
 
 def calculate_book_rental_charge(book, days_borrowed=0):
-    if days_borrowed:
-        if days_borrowed > 0:
-            return book.get_rental_charge(days_borrowed)
-
-    return 0
+    return book.get_rental_charge(days_borrowed) if days_borrowed > 0 else 0
 
