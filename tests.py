85caf9e9105a26066f75d541040505ef5eaac146
Mukewa O. Wekalao <onesmus.mukewa@gmail.com>
2021-04-07 05:56:59 +0300

    Adds tests for all types of books

--- tests.py
+++ tests.py
@@ -5,17 +5,40 @@ from utils import calculate_book_rental_charge
 
 
 class BooksTestCase(unittest.TestCase):
-    def test_regular_book_total_rental_charge(self):
+    def setUp(self):
         self.customer = Customer(
             name="Customer #1")
+
+        self.days_borrowed = 10
+
+
+    def test_regular_book_total_rental_charge(self):
         self.book = Book(
             name="Fundamentals of Accounting",
             kind=Book.REGULAR)
 
-        self.days_borrowed = 10
+        charge = calculate_book_rental_charge(self.book, days_borrowed=self.days_borrowed)
+        self.assertEqual(charge, 15)
+
+    def test_fiction_book_total_rental_charge(self):
+        self.customer = Customer(
+            name="Customer #1")
+        self.book = Book(
+            name="Wolf Hall",
+            kind=Book.FICTION)
 
         charge = calculate_book_rental_charge(self.book, days_borrowed=self.days_borrowed)
+        self.assertEqual(charge, 30)
 
+    
+    def test_novel_total_rental_charge(self):
+        self.customer = Customer(
+            name="Customer #1")
+        self.book = Book(
+            name="Lord of Flies",
+            kind=Book.NOVEL)
+
+        charge = calculate_book_rental_charge(self.book, days_borrowed=self.days_borrowed)
         self.assertEqual(charge, 15)
 
     
